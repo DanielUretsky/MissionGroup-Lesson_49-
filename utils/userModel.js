214480f8dd93fde4b16027ad07158a4c6dc401c7
Daniel <daniel353a3@gmail.com>
2023-12-07 20:59:37 +0200

    started more functionallity

--- utils/userModel.js
+++ utils/userModel.js
@@ -1,5 +1,9 @@
 export class User {
     constructor(id, firstname, lastname, email, password) {
-        
+        this.id = id,
+        this.firstname = firstname,
+        this.lastname = lastname,
+        this.email = email,
+        this.password = password
     }
 }
