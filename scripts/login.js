a243c02507a8f56acd908758b6aadc838682ce4d
RRazY <razyya@gmail.com>
2023-12-10 15:21:18 +0200

    notihng special

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,32 @@
 import { fetchUsers } from "../utils/fetchUsers.js";
 
-let usersArr=[];
+document.getElementById("LogBtn").addEventListener("click",logIn);
 
 ///varibales
+let usersArr=[];
+
+
+///////////////
+
 fetchUsers().then((data)=>{
     usersArr.push(...data);
-    console.log("line 8 ",usersArr);
+    // console.log("line 8 ",usersArr);
 
 })
 
 
 console.log("line 13 ",usersArr);
+
+function logIn(){
+let userEmail=document.getElementById("userMail");
+let userPass=document.getElementById("pasNum");
+console.log(userEmail.value);
+console.log(userPass.value);
+for(let x in usersArr){
+    if(x%4==0)
+    {
+        
+    }
+}
+
+}
