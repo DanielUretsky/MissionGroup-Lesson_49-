57b10a2a05d50479ee0a9bf3e53a1006cdc4d261
RRazY <razyya@gmail.com>
2023-12-10 18:48:04 +0200

    login.js change

--- scripts/login.js
+++ scripts/login.js
@@ -1,32 +1,46 @@
 import { fetchUsers } from "../utils/fetchUsers.js";
 
-document.getElementById("LogBtn").addEventListener("click",logIn);
 
 ///varibales
 let usersArr=[];
+let userCheck=false;
 
-
-///////////////
-
+/////////////// 9uQFF1Lh ___ atuny0@sohu.com
 fetchUsers().then((data)=>{
     usersArr.push(...data);
-    // console.log("line 8 ",usersArr);
 
 })
-
 
 console.log("line 13 ",usersArr);
 
 function logIn(){
+    let flag=true;
 let userEmail=document.getElementById("userMail");
 let userPass=document.getElementById("pasNum");
 console.log(userEmail.value);
 console.log(userPass.value);
 for(let x in usersArr){
-    if(x%4==0)
+    if(usersArr[x].email==userEmail.value&&usersArr[x].password==userPass.value)
     {
-        
+        let currentUser=usersArr[x];
+        localStorage.setItem("Current_User",JSON.stringify(currentUser))
+        flag=false;
+        window.location.href="../pages/home.html";
+        break;
+
+    }
+    if(x==usersArr.length-1&&flag==true)
+        alert("User not found . Check your details")
     }
 }
 
+
+
+const backReg= () =>
+{
+    window.location.href="../pages/registration.html";
 }
+
+//BTNs
+document.getElementById("LogBtn").addEventListener("click",logIn);
+document.getElementById("regBtn").addEventListener("click",backReg);
